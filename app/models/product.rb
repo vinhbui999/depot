524f7ad8d57031ba58fb53330ab066b62eb380f5
Bùi Xuân Vĩnh <buixuanvinh@Vinhs-MacbookPro.local>
2021-07-01 13:44:10 +0700

    Done validate

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+    validates :title, :image_url, :description, presence: true
+    validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+    validates :title, uniqueness: true
+    validates :image_url, allow_blank: true, format:{
+        with: %r/\.(gif|jpg|png)\Z/i,
+        message: "Must be a url for gif, jpg or png image."
+    }
 end
