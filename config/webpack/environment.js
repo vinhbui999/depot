5743b802dc6511e3da410f5ea8d9db4367057c17
vinhbui999 <vinhbui268@gmail.com>
2021-08-28 09:52:25 +0700

    updated deploy heroku

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,3 @@
-const { environment } = require("@rails/webpacker");
-environment.plugins.prepend(
-  "Provide",
-  new webpack.ProvidePlugin({
-    $: "jquery/src/jquery",
-    jQuery: "jquery/src/jquery"
-  })
+const { environment } = require('@rails/webpacker')
 
-);
-module.exports = environment;
+module.exports = environment
